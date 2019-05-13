14a7200893134b2c38c498a0a4e422f72334d03b
chengang <1410488687@qq.com>
2019-05-13 18:08:18 +0800

    图片库1.0.1 增加bitmap gif加载回调

---
+++ lib_imageloader/src/main/java/com/nj/baijiayun/imageloader/target/GifTarget.java
@@ -0,0 +1,12 @@
+package com.nj.baijiayun.imageloader.target;
+
+/**
+ * @author chengang
+ * @date 2019/5/13
+ * @email chenganghonor@gmail.com
+ * @QQ 1410488687
+ * @package_name com.nj.baijiayun.imageloader.target
+ * @describe
+ */
+public class GifTarget {
+}
